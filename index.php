aca0c21113c2eff83286fb9c940a78be6f292bd4
Trizia Salvador <fg88pn@onsemi.com>
2017-09-22 10:06:46 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge;IE=9;IE=8;IE=7">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>TPED Yield Monitoring</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<div id="header">
+<a href="http://10.153.214.138:81/"><img src="img/onsemi.png" style="height: 70px; width:200px;" class="logo"></a>
+</div>
+<div id="logo">
+<img src="img/xfn.png" style="margin-top:100px; margin-left:100px;"/>
+</div>
+<div id="container">
+		<form action="/action_page.php">
+	 <div class="login">
+  Username: <input type="text" name="user" style="width=500px" placeholder="Input username"><br><br>
+  Password: <input type="password" name="pass" style="width=500px"><br><br>
+ <button class="submit" value="submit" style="vertical-align:middle"><span>Login</span></button>
+</form>
+</div>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge;IE=9;IE=8;IE=7">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>TPED Yield Monitoring</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+<div id="container1">
+
+<div id="pamganda">
+YIELD MONITORING
+<a href="index.php" class="logout">Logout</a>
+<a>
+</div>
+<div id="line">
+</div>
+</div>
+</div>
+</body>
+
+
+</html>
